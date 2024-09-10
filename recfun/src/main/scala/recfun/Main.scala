4f638d8afcb159ee0b0b310be956115545662004
joon363 <joon363@naver.com>
2024-09-10 12:08:47 +0900

    balance implement

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -9,6 +9,12 @@ object Main {
         print(pascal(col, row) + " ")
       println()
     }
+
+    println("balance test")
+    print(balance("(if (zero? x) max (/ 1 x))".toList) + " should be true\n")
+    print(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList)+ " should be true\n")
+    print(balance(":-)".toList)+ " should be false\n")
+    print(balance("())(".toList)+ " should be false\n")
   }
 
   /**
@@ -23,9 +29,24 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def check(x: Int, auxList: List[Char]): Boolean = {
+      if (x < 0) false
+      else if (auxList.isEmpty) x == 0
+      else {
+        check(count(x, auxList.head), auxList.tail)
+      }
+    }
+    def count(num: Int, element: Char): Int = {
+      if (element == '(') num + 1
+      else if (element == ')') num - 1
+      else num
+    }
 
-  /**
+    check(0, chars)
+  }
+
+    /**
    * Exercise 3
    */
   def countChange(money: Int, coins: List[Int]): Int = ???
