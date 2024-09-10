c85cb384e8e774aedd112fa095af0f58d85461aa
joon363 <joon363@naver.com>
2024-09-10 12:15:43 +0900

    countChange implement

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,6 +15,13 @@ object Main {
     print(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList)+ " should be true\n")
     print(balance(":-)".toList)+ " should be false\n")
     print(balance("())(".toList)+ " should be false\n")
+
+    println("count test")
+    print(countChange(4,List(1,2))+ " should be 3\n")
+    print(countChange(2,List(1,2))+ " should be 2\n")
+    print(countChange(2,List(1,3))+ " should be 1\n")
+    print(countChange(2,List(5,3))+ " should be 0\n")
+    print(countChange(0,List(1,2))+ " should be 1\n")
   }
 
   /**
@@ -56,5 +63,18 @@ object Main {
     /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    // sort coins list in descending order
+    val sortedCoins = coins.sorted.reverse
+    // return 0 if no more coins available
+    if(sortedCoins.isEmpty && money!=0) 0
+    // return 1 if money is zero
+    else if(money==0) 1
+    // exclude coin which has bigger value than remaining money
+    else if (sortedCoins.head > money) countChange(money, sortedCoins.tail)
+    // first way: use biggest coin, recursion
+    // second way: do not use biggest coin, recursion without it
+    else countChange(money - sortedCoins.head, sortedCoins) +
+         countChange(money, sortedCoins.tail)
+  }
 }
