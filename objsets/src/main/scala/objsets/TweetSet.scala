52f37b110eeb18dee27c7ba9449fceb45daefbd5
joon363 <joon363@naver.com>
2024-09-30 14:54:25 +0900

    implemented fliter

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -42,7 +42,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-  def filter(p: Tweet => Boolean): TweetSet = ???
+  def filter(p: Tweet => Boolean): TweetSet = this.filterAcc(p,new Empty)
 
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -110,8 +110,7 @@ abstract class TweetSet {
 
 class Empty extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc;
 
   /**
    * The following methods are already implemented
@@ -128,8 +127,11 @@ class Empty extends TweetSet {
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
-
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = {
+    // recursively add elements from right to left
+    val newAcc = if(p(elem)) acc.incl(elem) else acc
+    left.filterAcc(p,right.filterAcc(p,newAcc))
+  }
 
   /**
    * The following methods are already implemented
